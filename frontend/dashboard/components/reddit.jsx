8c880e7f6a3fd9cacf6c1b6512246fc7cf61ede0
Robert Schneiderman <robert.a.schneiderman@gmail.com>
2017-02-09 00:38:28 -0800

    full styling for reddits

--- frontend/dashboard/components/reddit.jsx
+++ frontend/dashboard/components/reddit.jsx
@@ -8,11 +8,13 @@ class Reddit extends Component {
     }
 
     hoursAgo(timestamp) {
-        return moment().get('hours') - moment(timestamp).get('hours');
+        // debugger;
+        // return moment().get('date') * 24 + moment().get('hours') - moment(timestamp).get('date') + moment(timestamp).get('hours');
+        return moment.utc(moment().diff(moment(timestamp))).format('HH');
     }
     
     render() {
-        let { thumbnail, title, link_flair_text, num_comments, domain, author, subreddit } = this.props;
+        let { thumbnail, created_utc, title, link_flair_text, num_comments, domain, author, subreddit } = this.props;
         thumbnail = (thumbnail === 'self' || thumbnail === 'default') ? './static/images/self.png' : thumbnail;
         return(
             <div className="reddit c-reddit">
@@ -22,9 +24,9 @@ class Reddit extends Component {
                     <div className="text-wrapper">
                         <h3 className="title">{title} <span className="tstyle-box">{link_flair_text}</span> <a className="link">({domain})</a></h3>
                         <div className="below-title fb">
-                            <img src="" className="btn-media" />
+                            <svg xmlns="http://www.w3.org/2000/svg" className="btn-media" viewBox="0 0 34.9 34.9"><path d="M31.9 34.9h-29C1.3 34.9 0 33.6 0 32V3c0-1.7 1.3-3 2.9-3h29c1.6 0 2.9 1.3 2.9 2.9v29c.1 1.7-1.2 3-2.9 3z"/><path fill="#FFF" d="M7.8 8.7v17c0 2.2 2.4 3.6 4.3 2.5l14.8-8.5c1.9-1.1 1.9-3.9 0-5L12.1 6.2c-1.9-1.1-4.3.3-4.3 2.5zM32 27h-2c-.3 0-.5-.2-.5-.5v-2c0-.3-.2-.5-.5-.5h-1.3c-.3 0-.5.2-.5.5v2c0 .3-.2.5-.5.5h-2c-.3 0-.5.2-.5.5v1.3c0 .3.2.5.5.5h2c.3 0 .5.2.5.5v2c0 .3.2.5.5.5H29c.3 0 .5-.2.5-.5v-2c0-.3.2-.5.5-.5h2c.3 0 .5-.2.5-.5v-1.3c.1-.3-.2-.5-.5-.5z"/></svg>
                             <div className="side-of-media-btn">
-                                <p className="subtitle-reddit">submitted fillin hours ago by <a class="link-regular">{author}</a>  to /r/{subreddit}</p>
+                                <p className="subtitle">submitted {this.hoursAgo(created_utc)} hours ago by <a className="link-regular mr05">{author}</a>  to /r/{subreddit}</p>
                                 <div className="meta-data">
                                     <a href="" className="link-comments link-reddit">{num_comments} comments</a>
                                     <a href="" className="link-share link-reddit">share</a>
