18baf108fdd5687cc9fc71126a6540998cec01c8
Robert Schneiderman <robert.a.schneiderman@gmail.com>
2017-02-10 01:47:55 -0800

    added postinstall

--- frontend/utils/axios_defaults.js
+++ frontend/utils/axios_defaults.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = (process.env.NODE_ENV !== "production") ? 'http://localhost:3090' : 'https://trackyy.herokuapp.com';
+const baseURL = (process.env.NODE_ENV !== "production") ? 'http://localhost:3090' : 'https://reddit-lite2.herokuapp.com';
 axios.defaults.headers.common['x-auth'] = localStorage.getItem('token');
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 axios.defaults.baseURL = baseURL;
