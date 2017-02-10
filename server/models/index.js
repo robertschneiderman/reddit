7407d8e50741eaa6a9d7a1d0512234b3ec8698ef
Robert Schneiderman <robert.a.schneiderman@gmail.com>
2017-02-10 03:42:42 -0800

    trying easier solution

--- server/models/index.js
+++ server/models/index.js
@@ -11,16 +11,7 @@ var db        = {};
 var match = process.env.DATABASE_URL.match(/postgres:\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)/);
 
 if (process.env.DATABASE_URL) {
-  var sequelize = new Sequelize(match[5], match[1], match[2], {
-    dialect:  'postgres',
-    protocol: 'postgres',
-    port:     match[4],
-    host:     match[3],
-    logging: false,
-    dialectOptions: {
-        ssl: true
-    }
-  });
+  var sequelize = new Sequelize('postgres://cyszqvkycdmlnq:3aee60eed0a1c42d235337b01db884e48483e37a492b014ea3617902fc0b713a@ec2-54-163-234-20.compute-1.amazonaws.com:5432/d8ftjuju5muiek');
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
